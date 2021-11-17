8cae0991c18b44da98b5b2a6da88eec3f96eb6d0
vafonkin <vadim.afonkin@gmail.com>
2021-11-16 19:49:14 -0500

    Update tsservice.js

--- bindid-react-example/src/ts/tsservice.js
+++ bindid-react-example/src/ts/tsservice.js
@@ -4,8 +4,12 @@ export default class TransmitService {
     // Always use the API behavior of the latest version - UseLatest
     this.apiCompat = window.XmBindId.XmBindIdApiCompatibilityLevel.UseLatest;
     // Configure the BindID SDK with your client ID, and to work with the BindID sandbox environment
-    this.clientId = "";
-    this.redirectUri = "";
+    this.clientId = process.env.REACT_APP_BINDID_CLIENT_ID;
+    this.redirectUri = process.env.REACT_APP_REDIRECT_URI;
+    this.tokenUrl = process.env.REACT_APP_SERVER_TOKEN_URL;
+    this.home = process.env.REACT_APP_HOME_URI;
+    this.validationUrl = process.env.REACT_APP_SERVER_VALIDATE_URL;
+    this.jwksUrl = process.env.REACT_APP_JWKS_URL;
   }
 
   // SDK Initialization:
@@ -24,8 +28,12 @@ export default class TransmitService {
       console.log("AUTHENTICATE WITH BIND SDK", this.redirectUri);
       this.XmBindId.authenticate({
         redirectUri: this.redirectUri,
-        nonce: this.getRandomInt(),
-        state: String(this.getRandomInt()),
+        nonce:
+          window.localStorage.getItem("XM_BIND_ID_NONCE_KEY") ||
+          this.getRandomInt("nonce"),
+        state:
+          window.localStorage.getItem("XM_BIND_ID_STATE_KEY") ||
+          this.getRandomInt("state"),
       });
       //  Handle Error:
       // Display an error message to the user
@@ -35,7 +43,21 @@ export default class TransmitService {
     }
   }
 
-  getRandomInt() {
-    return Math.floor(Math.random() * (Math.pow(2, 31) - 1));
+  getRandomInt(key) {
+    const random = Math.floor(Math.random() * (Math.pow(2, 31) - 1));
+
+    switch (key) {
+      case "nonce":
+        window.localStorage.setItem("XM_BIND_ID_NONCE_KEY", random);
+        break;
+      case "state":
+        window.localStorage.setItem("XM_BIND_ID_STATE_KEY", random);
+        break;
+
+      default:
+        break;
+    }
+
+    return random;
   }
 }
